8f4175f55fccdbcee811b0c2007c76612c5ddb02
Brymo <bryanmoh@yahoo.com.au>
2019-03-24 20:31:33 +1100

    New site

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './components/components.css';
+import './App.css'
+import Name from './components/name'
+import Nav from './components/nav'
+
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+  }
+
   render() {
+    //const {caption} = this.state;
     return (
-      <div className="App">
-      Hello!
+      <div className="content">
+        <Name/>
+        <Nav/>
       </div>
-    );
+    )
   }
 }
 
