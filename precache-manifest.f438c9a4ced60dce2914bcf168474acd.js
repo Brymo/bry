1cd6aa66d4c7776aebd8e7ce7e80f97858d7f078
Brymo <bryanmoh@yahoo.com.au>
2019-04-20 23:13:36 +1000

    Updates

---
+++ precache-manifest.f438c9a4ced60dce2914bcf168474acd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6af7c0a2d4472dad47f3b05651fa720",
+    "url": "/bry/static/media/aboutme.a6af7c0a.md"
+  },
+  {
+    "revision": "6137cee3705d5e8dce08",
+    "url": "/bry/static/css/main.2bf882ce.chunk.css"
+  },
+  {
+    "revision": "4006ce3319490b45be8f",
+    "url": "/bry/static/js/runtime~main.ddf08eb5.js"
+  },
+  {
+    "revision": "0f00ea85c372b03f2607abb38553a59a",
+    "url": "/bry/static/media/mrx.0f00ea85.jpg"
+  },
+  {
+    "revision": "f544c9c0d7459ad45116",
+    "url": "/bry/static/js/2.e722bb9f.chunk.js"
+  },
+  {
+    "revision": "b03a9848775c7d88a95710c5a9487f11",
+    "url": "/bry/static/media/DAYROM.b03a9848.woff"
+  },
+  {
+    "revision": "247ed841f002fd9c6b68b7ca3a0b64ac",
+    "url": "/bry/static/media/coolvetica rg.247ed841.woff"
+  },
+  {
+    "revision": "6137cee3705d5e8dce08",
+    "url": "/bry/static/js/main.9fdbf70e.chunk.js"
+  },
+  {
+    "revision": "588432304af38077ff77d93c43e218bb",
+    "url": "/bry/static/media/fancy_me.58843230.jpg"
+  },
+  {
+    "revision": "553b709c03aac7407bfc51d1daf32051",
+    "url": "/bry/static/media/cooked0.553b709c.md"
+  },
+  {
+    "revision": "0e5084ce5e37628df02f2f4f549ed709",
+    "url": "/bry/static/media/cooked1.0e5084ce.md"
+  },
+  {
+    "revision": "7284474095f8f5e139de9e94c34707fd",
+    "url": "/bry/static/media/cooked2.72844740.md"
+  },
+  {
+    "revision": "f544c9c0d7459ad45116",
+    "url": "/bry/static/css/2.9b2564bb.chunk.css"
+  },
+  {
+    "revision": "b5231d9322fc979d0863e251ec510b8b",
+    "url": "/bry/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bry/precache-manifest.07eaa7629855fa8d973996fc2d818a48.js"
+  "/bry/precache-manifest.f438c9a4ced60dce2914bcf168474acd.js"
 );
 
 workbox.clientsClaim();
