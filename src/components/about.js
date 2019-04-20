896fafdeb81b14292e0e48b8d6d2a452432ff3e5
Brymo <bryanmoh@yahoo.com.au>
2019-04-20 23:25:19 +1000

    20/4/19

--- src/components/about.js
+++ src/components/about.js
@@ -1,71 +1,161 @@
-import React, { Component } from 'react';
-import './components.css';
-import {Drawer} from "antd";
+import React, { Component } from "react";
+import "./components.css";
+import { Drawer, Spin } from "antd";
 import ReactMarkdown from "react-markdown";
-
-
+import DrawNav from "./draw_nav";
 
 class About extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-        visible: false,
-        terms: null
-    }
+      visible: false,
+      terms: [],
+      articleId: 0,
+      loading: false
+    };
   }
 
-  componentWillMount(){
-    const readmePath = require("../blogs/aboutme.md");
+  format = text => {
+    const lines = text.split("\n");
+    let regex = /\((.*)\)\s*$/g;
+    const title = lines[0].match(regex)[0].replace(/^\(/g,'').replace(/\)\s*$/g,'');
+    const date = lines[1].match(regex)[0].replace(/^\(/g,'').replace(/\)\s*$/g,'');
+    return { title: title, date: date, text: text };
+  };
 
-    fetch(readmePath).then((response) => response.text()).then(text =>{
-      this.setState({terms:text})
-    })
+  componentWillMount() {
+    const { terms } = this.state;
+    this.startLoad();
+
+    (async () => {
+      let readmePath = null;
+      let i = 0;
+      let fileExists = true;
+      let allBlogs = [];
+
+      while (fileExists) {
+        try {
+          readmePath = require(`../blogs/aboutme/aboutme${i}.md`);
+          await fetch(readmePath)
+            .then(response => response.text())
+            .then(text => {
+            
+              allBlogs.push(this.format(text));
+     
+            });
+          i++;
+        } catch (e) {
+          fileExists = false;
+          console.log(e);
+        }
+      }
+
+      this.setState({ terms: allBlogs,articleId:i-1 });
+
+      this.stopLoad();
+
+    })();
   }
+
+  startLoad = () => {
+    this.setState({
+      loading: true
+    });
+  };
+
+  stopLoad = () => {
+    this.setState({
+      loading: false
+    });
+  };
 
   showDrawer = () => {
     this.setState({
-      visible: true,
+      visible: true
     });
   };
 
   onClose = () => {
     this.setState({
-      visible: false,
+      visible: false
     });
   };
 
+  nextArticle = () => {
+    this.setState({
+      articleId: this.state.articleId + 1
+    });
+  };
+
+  prevArticle = () => {
+    this.setState({
+      articleId: this.state.articleId - 1
+    });
+  };
+
+  getPrevNext = () => {
+    const { terms, articleId } = this.state;
+    const numArticles = terms.length;
+    const prevNext = { prev: null, next: null };
+
+    if (numArticles <= 1) {
+      return prevNext;
+    } else {
+      if (articleId === 0) {
+        prevNext.next = terms[1].title;
+      } else if (articleId === numArticles - 1) {
+        prevNext.prev = terms[numArticles - 2].title;
+      } else {
+        prevNext.next = terms[articleId + 1].title;
+        prevNext.prev = terms[articleId - 1].title;
+      }
+    }
+
+    return prevNext;
+  };
+
   render() {
-    //const {caption} = this.state;
+    const { terms, articleId, loading } = this.state;
 
-
-    const pic = require("../pics/fancy_me.jpg");
     return (
-        <div className="navItemContainer">
+      <div className="navItemContainer">
         <button className="smallName navitem" onClick={this.showDrawer}>
-            About Me
+          About Me
         </button>
         <Drawer
-          title="About Me"
+          title="Who am I?"
           placement="left"
           closable={true}
-          width = "55%"
+          width="70%"
           onClose={this.onClose}
           visible={this.state.visible}
-         
         >
-          <div className="blog">
-            <div className="title">
-                Hello there friend!
+          {loading ? (
+            <div style={{ textAlign: "center" }}>
+              <Spin size="large" />
             </div>
-            <img src={pic} className="picture"/>
-            <div className="date"><i>Written by Bryan Moh || 24.3.2019</i></div>
-            <ReactMarkdown source={this.state.terms} className="blogtext"/>
-          </div>
-        </Drawer>
+          ) : (
+            <div className="blog">
+              <DrawNav
+                prevNext={this.getPrevNext()}
+                nextArticle={this.nextArticle}
+                prevArticle={this.prevArticle}
+              />
+              <div className="title">{terms[articleId].title}</div>
 
-        </div>
-    )
+              <div className="date">
+                <i>Written by Bryan Moh || {terms[articleId].date}</i>
+              </div>
+              <ReactMarkdown
+                source={terms[articleId].text}
+                className="blogtext"
+              />
+            </div>
+          )}
+          
+        </Drawer>
+      </div>
+    );
   }
 }
 
--- src/components/draw_nav.js
+++ src/components/draw_nav.js
@@ -22,6 +22,7 @@ export default function DrawNav(props) {
       ) : (
         <div />
       )}
+
     </div>
   );
 }
