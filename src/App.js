f1640fc1295edf76bed90bca59fe17c9b198dff2
Brymo <bryanmoh@yahoo.com.au>
2019-04-25 01:09:55 +1000

    New blog!

--- src/App.js
+++ src/App.js
@@ -1,90 +1,90 @@
-import React, { Component } from 'react';
-import './components/components.css';
-import './App.css'
-import Name from './components/name'
-import Nav from './components/nav'
+import React, { Component } from "react";
+import "./components/components.css";
+import "./App.css";
+import Name from "./components/name";
+import Nav from "./components/nav";
 import { Spin } from "antd";
 
-
-
 class App extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state ={
-      loading:true,
-      latest:null
-    }
+    this.state = {
+      loading: true,
+      latest: null
+    };
   }
 
-  componentWillMount(){
-        
+  componentWillMount() {
     (async () => {
-      const folderNames = ["About Me","Cooked","Places","Code"];
-      let newestPost = {newestName:null, newestDate:-1};
+      const folderNames = ["About Me", "Cooked", "Places", "Code"];
+      let newestPost = { newestName: null, newestDate: -1 };
 
       let i = 0;
-      
 
-      while(i < folderNames.length){
+      while (i < folderNames.length) {
         const dateString = await this.getLatestPostDate(folderNames[i]);
         const dateValue = await this.dateToValue(dateString);
-        if (dateValue > newestPost.newestDate){
+        if (dateValue > newestPost.newestDate) {
           newestPost.newestName = folderNames[i];
           newestPost.newestDate = dateValue;
         }
         i++;
       }
-      
 
       this.setLatest(newestPost.newestName);
       this.setLoading(false);
-
-    })()
-
-
-
+    })();
   }
 
-  setLoading = (isLoading) => {
-    this.setState({loading:isLoading});
-  }
+  setLoading = isLoading => {
+    this.setState({ loading: isLoading });
+  };
 
-  setLatest = (latest) => {
-    this.setState({latest:latest});
-  }
+  setLatest = latest => {
+    this.setState({ latest: latest });
+  };
 
   getDate = text => {
     const lines = text.split("\n");
     let regex = /\((.*)\)\s*$/g;
-    const date = lines[1].match(regex)[0].replace(/^\(/g,'').replace(/\)\s*$/g,'');
+    const date = lines[1]
+      .match(regex)[0]
+      .replace(/^\(/g, "")
+      .replace(/\)\s*$/g, "");
     return date;
   };
 
-  dateToValue =  (dateString) => {
+  dateToValue = dateString => {
     const dayMonthYear = dateString.split(".");
-    return parseInt(dayMonthYear[2],10)*10000 + parseInt(dayMonthYear[1],10) * 100 + parseInt(dayMonthYear[0],10)
-  }
+    return (
+      parseInt(dayMonthYear[2], 10) * 10000 +
+      parseInt(dayMonthYear[1], 10) * 100 +
+      parseInt(dayMonthYear[0], 10)
+    );
+  };
 
-  grabFile =  async (index, folderName) => {
-    const Octokit = require('@octokit/rest');
-    const octokit = new Octokit({userAgent:'Midnight Laundry'});
+  grabFile = async (index, folderName) => {
+    const Octokit = require("@octokit/rest");
+    const octokit = new Octokit({
+      userAgent: "Midnight Laundryoctokit/rest.js v16.25.0",
+      auth: "99eece022093681ddd1f31f2e2cf341bdd5061a2"
+    });
     const stuff = await octokit.repos.getContents({
-      owner:"Brymo",
-      repo:"bry",
-      path:`./src/blogs/${folderName}/${folderName}${index}.md`
-    })
-    const content = await Buffer.from(stuff.data.content,'Base64').toString();
+      owner: "Brymo",
+      repo: "bry",
+      path: `./src/blogs/${folderName}/${folderName}${index}.md`
+    });
+    const content = await Buffer.from(stuff.data.content, "Base64").toString();
     return content;
-  }
+  };
 
-  getLatestPostDate =  async (folderName) => {
+  getLatestPostDate = async folderName => {
     let i = 0;
     let grabbedText = null;
     let fileExists = true;
 
     //find the latest post in the folder
-    while(fileExists === true){
+    while (fileExists === true) {
       try {
         grabbedText = await this.grabFile(i, folderName);
         i++;
@@ -94,37 +94,43 @@ class App extends Component {
     }
 
     //if no files in folder
-    if(i === 0){
+    if (i === 0) {
       return "0.0.0";
     }
 
     //fetch date from latest file
-    grabbedText = await this.grabFile(i-1,folderName);
+    grabbedText = await this.grabFile(i - 1, folderName);
     const date = await this.getDate(grabbedText);
     return date;
-
-  }
+  };
 
   render() {
     //const {caption} = this.state;
-    const {loading,latest} = this.state;
+    const { loading, latest } = this.state;
 
-    if(loading){
-      return(
-      <div style={{ display:"flex", justifyContent:"center", alignItems:"center", height:"100%" }}>
-        <Spin size="large" />
-      </div>)
+    if (loading) {
+      return (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100%"
+          }}
+        >
+          <Spin size="large" />
+        </div>
+      );
     }
 
     console.log(latest);
-      
+
     return (
       <div className="content">
-        <Name/>
-        <Nav latestPost={latest}/>
+        <Name />
+        <Nav latestPost={latest} />
       </div>
-    )
-    
+    );
   }
 }
 
